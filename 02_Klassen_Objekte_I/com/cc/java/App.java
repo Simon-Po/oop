cc4d3e3a46b84c743aa86977d463cd16716b7e45
Simon Pohl <simonnogam@gmail.com>
2022-11-22 09:30:06 +0100

    init cat

--- 02_Klassen_Objekte_I/com/cc/java/App.java
+++ 02_Klassen_Objekte_I/com/cc/java/App.java
@@ -3,7 +3,9 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-        output("die Zahl lautet" + 2);
+       Cat cat = new Cat();
+    
+
     }
 
 
---
+++ 02_Klassen_Objekte_I/com/cc/java/Cat.java
@@ -0,0 +1,5 @@
+package com.cc.java;
+
+public class Cat {
+    
+}
