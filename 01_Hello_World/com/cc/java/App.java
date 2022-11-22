880cd621e5d71f815da19af5c07004ff55f2e5dc
Simon Pohl <simonnogam@gmail.com>
2022-11-22 08:43:09 +0100

    initial

--- 01_Hello_World/com/cc/java/App.java
+++ 01_Hello_World/com/cc/java/App.java
@@ -3,6 +3,6 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-       System.out.println("Hello, World!"); 
+       System.out.println("Hello, World!");   
     }
 }
---
+++ 02_Klassen_Objekte_I/README.md
@@ -0,0 +1,1 @@
+# 02a_Klassen_Objekte
