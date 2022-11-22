a5dfd8dd7e72b501431f917050b348007283a57f
Simon Pohl <simonnogam@gmail.com>
2022-11-22 09:20:41 +0100

    initial

--- 02_Klassen_Objekte_I/com/cc/java/App.java
+++ 02_Klassen_Objekte_I/com/cc/java/App.java
@@ -3,8 +3,10 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        output("die Zahl lautet" + 2);
     }
+
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
