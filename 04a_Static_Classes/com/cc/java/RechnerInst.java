1f3fa900a033863d3f9c521748bf85ef48664b08
Simon Pohl <simonnogam@gmail.com>
2022-11-25 11:37:49 +0100

    intial

---
+++ 04a_Static_Classes/com/cc/java/RechnerInst.java
@@ -0,0 +1,22 @@
+package com.cc.java;
+
+public class RechnerInst {
+
+	int summe(int a, int b){
+		return a+b;
+	}
+	
+	int differenz(int a, int b){
+		return a-b;
+	}
+	
+	int produkt(int a, int b){
+		return a*b;
+	}
+	
+	double quotient(double a, double b){
+		return a/b;
+	}
+	
+
+} // EoC
