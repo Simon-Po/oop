f02fa5ea3be021c96ac2bf1eb5502d03ea78cca4
Simon Pohl <simonnogam@gmail.com>
2022-11-29 09:10:22 +0100

    done

--- 09b_ArrayLists/com/cc/java/App.java
+++ 09b_ArrayLists/com/cc/java/App.java
@@ -9,73 +9,73 @@ public class App {
 		
 		// ArrayList (AL) anlegen ...
 		
-		// ArrayList<String> carMakers = new ArrayList<>();
+		ArrayList<String> carMakers = new ArrayList<>();
 		
-		// System.out.println(carMakers);
-		// System.out.println(carMakers.size());
+		System.out.println(carMakers);
+		System.out.println(carMakers.size());
 		
 		// // AL dynamisch füllen (JS --> push() / J --> add())
-		// carMakers.add("Volvo");
-		// carMakers.add("BMW");
-		// carMakers.add("Ford");
-		// carMakers.add("Honda");
-		// carMakers.add("Tesla");
+		carMakers.add("Volvo");
+		carMakers.add("BMW");
+		carMakers.add("Ford");
+		carMakers.add("Honda");
+		carMakers.add("Tesla");
 		
-		// System.out.println(carMakers);
-		// System.out.println(carMakers.size());
+		System.out.println(carMakers);
+		System.out.println(carMakers.size());
 		
-		// // Daten aus AL lesen
-		// System.out.println(carMakers.get(0));
+		// Daten aus AL lesen
+		System.out.println(carMakers.get(0));
 		
-		// System.out.println("");
-		// System.out.println("------- Iteration 1 -----------");
+		System.out.println("");
+		System.out.println("------- Iteration 1 -----------");
 
-		// // Iteration 1
-		// for (int i = 0; i < carMakers.size(); i++) {
-		// 	System.out.print(carMakers.get(i) + " ");
-		// }
+		// Iteration 1
+		for (int i = 0; i < carMakers.size(); i++) {
+			System.out.print(carMakers.get(i) + " ");
+		}
 
-		// System.out.println("");
-		// System.out.println("------- Iteration 2 ---------");
+		System.out.println("");
+		System.out.println("------- Iteration 2 ---------");
 		
-		// // Iteration 2
-		// for (String element : carMakers) {
-		// 	System.out.print(element + " ");
-		// }	
+		// Iteration 2
+		for (String element : carMakers) {
+			System.out.print(element + " ");
+		}	
 		
 		// // Daten in AL ersetzen / Überschreiben
-		// System.out.println("");
-		// carMakers.set(1, "MB");
-		// System.out.println(carMakers);
+		System.out.println("");
+		carMakers.set(1, "MB");
+		System.out.println(carMakers);
 		
-		// System.out.println("");
-		// System.out.println("-------- sortiert: ------------");
+		System.out.println("");
+		System.out.println("-------- sortiert: ------------");
 		
-		// Collections.sort(carMakers);
-		// System.out.println(carMakers);
-		// System.out.println(carMakers.get(0));
+		Collections.sort(carMakers);
+		System.out.println(carMakers);
+		System.out.println(carMakers.get(0));
 		
-		// // Daten entfernen (JS --> pop(); J --> remove())
+		// Daten entfernen (JS --> pop(); J --> remove())
 		
-		// carMakers.remove(0);
-		// System.out.println(carMakers);
-		// carMakers.remove(carMakers.size()-1);
-		// System.out.println(carMakers);
+		carMakers.remove(0);
+		System.out.println(carMakers);
+		carMakers.remove(carMakers.size()-1);
+		System.out.println(carMakers);
 	
-		//  // AL leeren ...
-		// carMakers.clear();
-		// System.out.println(carMakers);
+		 // AL leeren ...
+		carMakers.clear();
+		System.out.println(carMakers);
 		
 		// System.out.println("");
 		// System.out.println("-------Thema: Wrapper -------------");
 		
-		// ArrayList<String> strList = new ArrayList<>();
-		// ArrayList<int> intList = new ArrayList<>();
+		ArrayList<String> strList = new ArrayList<>();
+		ArrayList<Integer> intList = new ArrayList<>();
 		// Lösung: Wrapper
 		
 		// int boo = 2;
-		// Integer boo = 2;
-		// System.out.println("Inhalt: " + boo.intValue());
+		Integer boo = 2;
+		System.out.println("Inhalt: " + boo.intValue());
 		
 		// ArrayList<Integer> intList = new ArrayList<>();
 		// intList.add(10);
