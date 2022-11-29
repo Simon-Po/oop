f7aaf05ffd42fa9899d683b82705bb62aa66a123
Simon Pohl <simonnogam@gmail.com>
2022-11-29 10:03:38 +0100

    looked at

--- 09c_ArrayLists_Cats/com/cc/java/App.java
+++ 09c_ArrayLists_Cats/com/cc/java/App.java
@@ -7,33 +7,33 @@ public class App {
 	public static void main(String[] args) {
 		
 		// Vorüberlegung
-		// Cat cat = new Cat("Grizabella");
-		// // output(cat.getAttributes());
+		Cat cat = new Cat("Grizabella");
+		// output(cat.getAttributes());
 		
-		//cat1 / cat2 .. more of the same ...DRY
-		// for (int i = 0; i < 10; i++) 
-		// {
-		// 	new Cat("someone_" + i); // Objekt, aber KEINE Referenz!
-		// }
-		
-		String [] nameArr = {"Grizabella","Alonzo", "RumTumTugger","Suzanne"};
-		ArrayList<Cat> objList = new ArrayList<>();
-
-		for (int i = 0; i < nameArr.length; i++) 
-		// for (int i = 0; i < 4; i++) 
+		// cat1 / cat2 .. more of the same ...DRY
+		for (int i = 0; i < 10; i++) 
 		{
-			objList.add(new Cat(nameArr[i]));
-			output("Cats: " + objList);
+			new Cat("someone_" + i); // Objekt, aber KEINE Referenz!
+		}
+		
+		// String [] nameArr = {"Grizabella","Alonzo", "RumTumTugger","Suzanne"};
+		// ArrayList<Cat> objList = new ArrayList<>();
 
-			objList.add(new Cat("someone_" + i));
+		// for (int i = 0; i < nameArr.length; i++) 
+		// // for (int i = 0; i < 4; i++) 
+		// {
+		// 	objList.add(new Cat(nameArr[i]));
+		// 	output("Cats: " + objList);
+
+		// 	objList.add(new Cat("someone_" + i));
 		
 
-		}
+		// }
 
-		output("Cats: " + objList);
-		output(objList.get(0).getAttributes());
-		output(objList.get(1).getAttributes());
-		output(objList.get(objList.size()-1).getAttributes());
+		// output("Cats: " + objList);
+		// output(objList.get(0).getAttributes());
+		// output(objList.get(1).getAttributes());
+		// output(objList.get(objList.size()-1).getAttributes());
 		
 
 	}
