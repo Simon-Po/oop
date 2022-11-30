3fd239ebd6e23b602dfd57ba78f5b468df4a82f5
Simon Pohl <simonnogam@gmail.com>
2022-11-30 08:43:58 +0100

    a

--- 07_Abstract_Classes/com/cc/java/App.java
+++ 07_Abstract_Classes/com/cc/java/App.java
@@ -4,6 +4,15 @@ public class App {
     
     public static void main(String[] args) {
      
+        Shape circle = new Circle(3);
+        Shape rectangle = new Rectangle(4, 12);
+        Shape triangle = new Triangle(21, 10);
+        output("------  Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area());
+
+
     }
 
     public static void output(String outputStr) {
